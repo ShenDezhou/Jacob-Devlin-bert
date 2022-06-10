c49f863ad46ed23becedbc52cbc2e75251b2114c
ShenDezhou <tsinghua9boy@sina.com>
2022-06-10 14:24:13 +0800

    1. pretrain bert for 32M bytes data.

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,7 +1,7 @@
 BERT_BASE_DIR="bert_model_cn/chinese_L-12_H-768_A-12"
 
 python run_pretraining.py \
-  --input_file=pretrain_cn_output/train_1.tfrecord \
+  --input_file=pretrain_cn_output/train.tfrecord \
   --output_dir=pretrain_cn_output/ \
   --do_train=True \
   --do_eval=True \
@@ -10,6 +10,6 @@ python run_pretraining.py \
   --train_batch_size=32 \
   --max_seq_length=128 \
   --max_predictions_per_seq=20 \
-  --num_train_steps=20 \
-  --num_warmup_steps=10 \
+  --num_train_steps=124000 \
+  --num_warmup_steps=6200 \
   --learning_rate=2e-5
